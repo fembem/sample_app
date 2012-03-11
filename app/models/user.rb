6a9040eec78aa8e285e6430bf3425eb21850800b
Leo <leo.decandia@gmail.com>
2012-03-11 11:13:24 -1000

    user signin working.

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,11 +19,16 @@ class User < ActiveRecord::Base
   end
 
   class << self
+    
     def authenticate(email, submitted_password)
       user = find_by_email(email)
       (user && user.has_password?(submitted_password)) ? user : nil
     end
     
+    def authenticate_with_salt(id, cookie_salt)
+      user = find_by_id(id)
+      (user && user.salt == cookie_salt) ? user : nil
+    end
 
   end
   
@@ -48,6 +53,7 @@ class User < ActiveRecord::Base
 end
 
 
+
 # == Schema Information
 #
 # Table name: users
@@ -58,5 +64,6 @@ end
 #  created_at         :datetime
 #  updated_at         :datetime
 #  encrypted_password :string(255)
+#  salt               :string(255)
 #
 
