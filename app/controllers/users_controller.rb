44698cf8e9bc985b1314523cf529486930f7114f
Leo <leo.decandia@gmail.com>
2012-03-11 12:28:37 -1000

    completed signin and signout.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
     #raise params[:user].inspect
     @user = User.new(params[:user])
     if @user.save
+      sign_in @user
       #flash[:success] = "Welcome to the sample app"
       redirect_to @user, :flash => { :success => "Welcome to the sample app" }
     else
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -35,6 +35,11 @@ Spork.prefork do
     config.use_transactional_fixtures = true
     
     ActiveSupport::Dependencies.clear
+    
+    def test_sign_in(user)
+      controller.current_user = user
+    end
+    
   end
 end
 
