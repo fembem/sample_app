7af69b5a1016e49f338b9fa5f31a0dd96d9261c2
Leo <leo.decandia@gmail.com>
2012-03-17 11:51:49 -1000

    admins can destroy users.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -154,8 +154,30 @@ describe User do
     end
     
   end
+  
+  describe "admin attribute" do
+    
+    before(:each) do
+      @user = User.create!(@attr)
+    end
+    
+    it "should respond to an admin" do
+      @user.should respond_to(:admin)
+    end
+    
+    it "should not be an admin by default" do
+      @user.should_not be_admin
+    end
+    
+    it "should be convertible to an admin" do
+      @user.toggle!(:admin)
+      @user.should be_admin
+    end
+    
+  end
     
 end
+
 
 
 
@@ -170,5 +192,6 @@ end
 #  updated_at         :datetime
 #  encrypted_password :string(255)
 #  salt               :string(255)
+#  admin              :boolean         default(FALSE)
 #
 
